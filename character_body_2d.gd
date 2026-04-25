bed4f7991809990e606cd848c33c48bbaeb652ee
FelipeFik11 <dedominicifelipe@gmail.com>
2026-04-24 21:09:53 -0300

    particulas(dash, caminar, ambiente)

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 @export var dash_speed = 600
 @export var dash_time = 0.2
 @export var dash_cooldown = 2
+var was_in_air
 var is_dashing = false
 var dash_timer = 0.0
 var dash_cooldown_timer = 2
@@ -16,8 +17,6 @@ var max_dash = 1
 var double_jump_used = false
 var jump_count = 0
 var max_jumps = 2
-
-
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		if velocity.y < 0:
@@ -31,10 +30,11 @@ func _physics_process(delta: float) -> void:
 			$AnimatedSprite2D.play("walk")	
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
-	
 	if is_dashing:
-		$AnimatedSprite2D.play("dash")			
-				
+		$AnimatedSprite2D.play("dash")	
+		
+		$DashParticles2D.restart()		
+	
 	if dash_cooldown_timer > 0: 
 		dash_cooldown_timer -= delta
 	if Input.is_action_just_pressed("dash") and dash_cooldown_timer <= 0:
@@ -44,31 +44,34 @@ func _physics_process(delta: float) -> void:
 		dash_direction = sign(velocity.x)
 		if dash_direction == 0:
 			dash_direction = 1 
-		
+
 	if is_dashing:
 		dash_timer -= delta
 		velocity.x = dash_direction * dash_speed
 		velocity.y = 0 
 		if dash_timer <= 0:
 			is_dashing = false
-		move_and_slide()
+		move_and_slide()	
 		return	
-	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		velocity.y = min(velocity.y, max_fall_speed)
 	else:
 		jump_count = 0
-	
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
+		velocity.y = - 400
+		$JumpParticles.restart()
+	$WalkParticles.emitting = abs(velocity.x) > 10 and is_on_floor()
+	if velocity.x != 0:
+		$WalkParticles.scale.x = -sign(velocity.x)	
+		
 	var direction = Input.get_axis("izquierda", "derecha")
 	if direction != 0:
 		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
 	else:
 		velocity.x = move_toward(velocity.x,0, friction * delta)
-	
 	if Input.is_action_just_pressed("saltar") and jump_count < max_jumps:
 		velocity.y = jump_force
 		jump_count += 1			
-
-	move_and_slide()
 	
+	move_and_slide()
